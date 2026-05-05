0ca304de7d329b3001aa9cf140aef875e2adc270
Enzo Wanderley Tomazoni <enzo.wanderley@academico.ifpb.edu.br>
2026-05-04 22:15:18 -0300

    criação da pasta data para conter os dados mockados e local storage

--- Clarify/src/main.js
+++ Clarify/src/main.js
@@ -1,12 +1,13 @@
 import './style.css'
 import * as aux from "./lib/funcoesAuxiliares.js"
 import { carregarRegistro, ativarListenerRegistro } from './pages/registro.js'
+import { dashboardcoord } from './pages/dashboardcoord.js';
 
 // OBS: é de extrema importância resolver tudo no sistema que comece com [OBS:] do programa na fase de entrega
 // Tudo que for temporário ou está sendo testado, é recomendado a utilização de [OBS:] pra facilitar a busca de coisas inúteis no futuro
 
 function main() {
-   document.querySelector('#app').innerHTML = carregarRegistro();
+   document.querySelector('#app').innerHTML = dashboardcoord();
    ativarListenerRegistro();
 }
 
