d8198ed5cbea423e577e9a9167a02d265eba26c8
Lucas-Vinicius-dev <cavalcanti.costa@academico.ifpb.edu.br>
2026-05-05 22:02:07 -0300

    feat: registro das URLs e melhoria geral no dashboard

--- Clarify/src/main.js
+++ Clarify/src/main.js
@@ -8,16 +8,16 @@ import * as nav from "./lib/navegacaoURL.js"
 // OBS: é de extrema importância resolver tudo no sistema que comece com [OBS:] do programa na fase de entrega
 // Tudo que for temporário ou está sendo testado, é recomendado a utilização de [OBS:] pra facilitar a busca de coisas inúteis no futuro
 
-window.addEventListener("popstate", () => {
-   nav.navigateURL(window.location.pathname);
-});
-
 function main() {
    aux.createKeys();
    aux.popularLocalStorage();
+
+   window.addEventListener("popstate", () => {
+      nav.navigateURL(window.location.pathname);
+   });
    
    if (localStorage.getItem('auth') === "true" && window.location.pathname === "/") {
-      window.history.replaceState({}, "", "/dashboardcoord");
+      aux.adicionarCaminhoURL("dashboardcoord");
    }
 
    nav.navigateURL(window.location.pathname);
