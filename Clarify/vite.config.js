344b1bfe0350e5b38bc34ec12d707ee913b35555
Francisco Josetti <franciscojosetti@gmail.com>
2026-05-12 22:49:49 -0300

    resolvendo merge com a develop 3

--- Clarify/vite.config.js
+++ Clarify/vite.config.js
@@ -3,8 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-<<<<<<< HEAD
-=======
-  //base: '/https://github.com/Lucas-Vinicius-dev/Clarify/'
->>>>>>> aabf5391a1852d29961c3c1916e683f6be296011
+  base: '/Clarify/'
 })
