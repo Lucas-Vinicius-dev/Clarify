b2dc520475a24f9382187c8fde198fc972872805
Gabriel Carvalho <ggoc.carvalho@gmail.com>
2026-06-23 15:43:34 -0300

    chore(clarify-next): remove dead code (UI kit, barrels e exports sem uso)
    Remove componentes UI nao importados (Button/Input/Label/Textarea), os
    barrels index.ts nao usados, funcoes mortas de lib/utils, sub-componentes
    de Card, tipoDemandaSchema, e os tipos Usuario/TIPOS_DEMANDA sem uso.

--- clarify-next/src/components/coordenador/index.ts
+++
@@ -1,6 +0,0 @@
-export { CardMetrica } from './CardMetrica';
-export { CardAluno } from './CardAluno';
-export { CardTurma } from './CardTurma';
-export { ModalCriarTurma } from './ModalCriarTurma';
-export { ModalFeedback } from './ModalFeedback';
-export { FormAdicionarAluno } from './FormAdicionarAluno';
